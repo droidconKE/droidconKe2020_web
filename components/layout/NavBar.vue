518213b983acde406b755d7dd26a72cb6d6625dc
manuelgeek <emashmagak@appslab.co.ke>
2020-04-30 19:20:04 +0300

    styled nav dropdown,fixed class stylings

--- components/layout/NavBar.vue
+++ components/layout/NavBar.vue
@@ -22,22 +22,23 @@
             <a class="py-2 px-4 inline-block text-gray-600 text-px-16 black no-underline nav-link" href="#"><i class="fa fa-bell" /> Sessions</a>
           </li>
           <li class="mr-3">
-            <a class="py-2 px-4 inline-block text-gray-600 no-underline text-px-16 nav-link " href="#"><i class="dr-star" /> About</a>
+            <a class="py-2 px-4 inline-block text-gray-600 no-underline text-px-16 nav-link " href="#"><i class="fa fa-star" /> About</a>
           </li>
           <li class="mr-3">
-            <a class="inline-block text-gray-600 no-underline  py-2 px-4 text-px-16 nav-link" href="#"> Sponsors</a>
+            <a class="inline-block text-gray-600 no-underline  py-2 px-4 text-px-16 nav-link" href="#"><i class="fa fa-star" /> Sponsors</a>
           </li>
         </ul>
       </div>
 
       <div class="w-4/12 flex-grow lg:flex justify-end">
         <div class="relative inline-block" @click="toggleDropDown" @keydown.enter="isVisible = !isVisible">
-          <button type="button" class="inline-flex items-center justify-between px-2 py-1 font-medium text-gray-700 transition-all duration-500 rounded-md focus:outline-none focus:text-brand-900 sm:focus:shadow-outline">
-            <span class="flex-shrink-0">Menu</span>
-            <svg fill="currentColor" viewBox="0 0 20 20" class="flex-shrink-0 w-5 h-5 ml-1">
-              <path :class="{ 'rotate-180': isVisible }" class="transition duration-300 ease-in-out origin-center transform" fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-            </svg>
-          </button>
+          <span class="cursor-pointer inline-flex items-center justify-between p-1 transition-all duration-500 rounded-full h-10 w-10 border bg-green-c">
+            <img src="/images/svg/maasai_male.svg" alt="icon">
+
+            <!--            <svg fill="currentColor" viewBox="0 0 20 20" class="flex-shrink-0 w-5 h-5 ml-1">-->
+            <!--              <path :class="{ 'rotate-180': isVisible }" class="transition duration-300 ease-in-out origin-center transform" fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />-->
+            <!--            </svg>-->
+          </span>
           <transition
             enter-active-class="transition duration-300 ease-out transform"
             enter-class="-translate-y-3 scale-95 opacity-0"
@@ -46,16 +47,25 @@
             leave-class="translate-y-0 opacity-100"
             leave-to-class="-translate-y-3 opacity-0"
           >
-            <div v-show="isVisible" class="absolute pt-2">
-              <div class="relative py-1 bg-white border border-gray-200 rounded-md shadow-xl">
-                <div class="absolute top-0 w-4 h-4 origin-center transform rotate-45 translate-x-5 -translate-y-2 bg-white border-t border-l border-gray-200 rounded-sm pointer-events-none" />
-                <div class="relative">
-                  <a href="#" class="block w-full px-4 py-2 font-medium text-gray-700 whitespace-no-wrap hover:bg-gray-100 focus:outline-none hover:text-gray-900 focus:text-gray-900 focus:shadow-outline transition duration-300 ease-in-out">Submenu Link #1</a>
-                  <a href="#" class="block w-full px-4 py-2 font-medium text-gray-700 whitespace-no-wrap hover:bg-gray-100 focus:outline-none hover:text-gray-900 focus:text-gray-900 focus:shadow-outline transition duration-300 ease-in-out">Submenu Link #2</a>
-                  <a href="#" class="block w-full px-4 py-2 font-medium text-gray-700 whitespace-no-wrap hover:bg-gray-100 focus:outline-none hover:text-gray-900 focus:text-gray-900 focus:shadow-outline transition duration-300 ease-in-out">Submenu Link #3</a>
-                  <a href="#" class="block w-full px-4 py-2 font-medium text-gray-700 whitespace-no-wrap hover:bg-gray-100 focus:outline-none hover:text-gray-900 focus:text-gray-900 focus:shadow-outline transition duration-300 ease-in-out">Submenu Link #4</a>
-                </div>
-              </div>
+            <div v-if="isVisible" id="userMenu" class=" rounded shadow-md mt-2 absolute mt-12 top-0 lg:right-0 min-w-full z-30">
+              <div class="absolute top-0 lg:right-0 lg:mr-10 w-2 h-2 mt-1 origin-center transform rotate-45 translate-x-5 -translate-y-2 pinn border-t rounded-sm pointer-events-none" />
+              <ul class="list-reset">
+                <li>
+                  <div class="flex-wrap flex p-2 items-center">
+                    <div class="w-1/2 flex-wrap flex border-r border-bg-black-1">
+                      <p><small class="logg">Logged in as</small></p>
+                      <p class="name">John Mwendwa</p>
+                    </div>
+                    <div class="w-1/2 flex-wrap flex justify-center">
+                      <a href="#"><i class="fa fa-sign-out green-dark"></i> <span class="name">Logout</span></a>
+                    </div>
+                  </div>
+                </li>
+                <li><hr class="border-t mx-2 border-bg-black-1"></li>
+                <li>
+                  <a href="#" class="px-4 py-2 block session-link hover:bg-gray-800 no-underline hover:no-underline"><i class="fa fa-star"></i> My Sessions</a>
+                </li>
+              </ul>
             </div>
           </transition>
         </div>
@@ -88,7 +98,7 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
   #nav-content {
     animation: 1s appear;
     margin: auto;
@@ -104,5 +114,47 @@ export default {
   }
   .nav-link:hover {
     color: var(--purple-color);
+  }
+  .active {
+    color: var(--purple-color);
+  }
+  .pinn {
+    background-color: #191D1D;
+  }
+  #userMenu, {
+    background-color: #191D1D;
+    .session-link {
+      text-align: left;
+      font-size: 16px;
+      line-height: 19px;
+      letter-spacing: 0px;
+      font-weight: lighter;
+      color: #FFFFFF;
+      opacity: 1;
+    }
+    .name {
+      text-align: left;
+      font-size: 14px;
+      line-height: 17px;
+      letter-spacing: 0px;
+      font-weight: lighter;
+      color: #FFFFFF;
+      opacity: 1;
+    }
+    .logg {
+      text-align: left;
+      font-size: 10px;
+      line-height: 13px;
+      font-weight: lighter;
+      letter-spacing: 0px;
+      color: #68DEA4;
+      opacity: 1;
+    }
+    .border-bg-black-1 {
+      border-color: #000;
+    }
+  }
+  .list-reset {
+    width: 230px;
   }
 </style>
