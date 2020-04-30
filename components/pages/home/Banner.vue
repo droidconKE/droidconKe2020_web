7c8cda2554195a08662bace74b63df7220506395
manuelgeek <emashmagak@appslab.co.ke>
2020-04-30 20:38:53 +0300

    home page partial design

--- components/pages/home/Banner.vue
+++ components/pages/home/Banner.vue
@@ -3,17 +3,17 @@
     <div class="container mx-auto flex flex-wrap bg-right bg-50% bg-no-repeat" style="background-image: url(https://demos.creative-tim.com/tailwindcss-starter-project/_next/static/images/ill_header_3-58248cf88527a12ccfc6cc70ec21d868.png)">
       <div class="w-full text-left content-start">
         <div class="w-full pt-10 sm:pt-0">
-          <h1 class="font-bold text-6xl text-gray-700">
+          <h1 class="title-large">
             droidconKe 2021
           </h1>
-          <h4 class="font-semibold text-gray-700">
+          <h4 class="title-px-39 purple">
             6TH TO 8TH AUGUST
           </h4>
-          <p class="mt-4 text-lg leading-relaxed text-gray-600">
+          <p class="mt-4 text-px-36 black leading-relaxed md:pt-10 ">
             LARGEST FOCUSED ANDROID <br> DEVELOPER CONFERENCE IN AFRICA
           </p>
           <div class="mt-12">
-            <a class="get-started text-white font-bold px-6 py-4 rounded outline-none focus:outline-none mr-1 mb-1 bg-pink-500 active:bg-pink-600 uppercase text-sm shadow hover:shadow-lg" style="transition:all .15s ease" href="/learning-lab/tailwind-starter-kit/documentation/download">Get Your Ticket</a>
+            <a class="button-yellow black text-px-16-b" href="#">Get Your Ticket</a>
           </div>
         </div>
       </div>
