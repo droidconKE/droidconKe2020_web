d031993ef5f72a37bfae72445df3f3395ee6d6ce
manuelgeek <emashmagak@appslab.co.ke>
2020-05-01 00:43:35 +0300

    events section

--- components/pages/home/EventTypes.vue
+++ components/pages/home/EventTypes.vue
@@ -1,46 +1,46 @@
 <template>
-  <section class="w-full h-auto bg-green-100 md:px-32 px-4">
+  <section class="w-full h-auto bg-green-c md:px-32 px-4">
     <div class="flex flex-wrap md:py-12">
       <div class="w-full text-center p-6 items-center justify-center">
-        <h3 class="text-4xl">
+        <h3 class="title-px-52 black">
           Event Types
         </h3>
       </div>
-      <div class="w-full items-center flex flex-wrap">
-        <div class="w-4/12 sm:w-4/12 md:w-2/12">
-          <img class="w-full bg-blue-300 p-6" src="/icon.png" alt="Sunset in the mountains">
+      <div class="w-full flex flex-wrap">
+        <div class="flex-1 w-4/12 sm:w-4/12 md:w-2/12">
+          <img class="w-full bg-yellow-c py-3 px-5 md:px-16 md:py-8 panel-img" src="/images/events/community.svg" alt="Sunset in the mountains">
           <div class="text-left">
-            <h6>Event</h6>
+            <h6 class="text-px-21 black">Community Day</h6>
           </div>
         </div>
-        <div class="w-4/12 sm:w-4/12 md:w-2/12">
-          <img class="w-full bg-blue-300 p-6" src="/icon.png" alt="Sunset in the mountains">
+        <div class="flex-1 w-4/12 sm:w-4/12 md:w-2/12">
+          <img class="w-full bg-dark-green-c py-3 px-5 md:px-16 md:py-8 panel-img" src="/images/events/work.svg" alt="Sunset in the mountains">
           <div class="text-left">
-            <h6>Event</h6>
+            <h6 class="text-px-21 black">Workshops</h6>
           </div>
         </div>
-        <div class="w-4/12 sm:w-4/12 md:w-2/12">
-          <img class="w-full bg-blue-300 p-6" src="/icon.png" alt="Sunset in the mountains">
+        <div class="flex-1 w-4/12 sm:w-4/12 md:w-2/12">
+          <img class="w-full bg-purple-c py-3 px-5 md:px-16 md:py-8 panel-img" src="/images/events/interviews.svg" alt="Sunset in the mountains">
           <div class="text-left">
-            <h6>Event</h6>
+            <h6 class="text-px-21 black">Interviews</h6>
           </div>
         </div>
-        <div class="w-4/12 sm:w-4/12 md:w-2/12">
-          <img class="w-full bg-blue-300 p-6" src="/icon.png" alt="Sunset in the mountains">
+        <div class="flex-1 w-4/12 sm:w-4/12 md:w-2/12">
+          <img class="w-full bg-yellow-c py-3 px-5 md:px-16 md:py-8 panel-img" src="/images/events/panels.svg" alt="Sunset in the mountains">
           <div class="text-left">
-            <h6>Event</h6>
+            <h6 class="text-px-21 black">Panels</h6>
           </div>
         </div>
-        <div class="w-4/12 sm:w-4/12 md:w-2/12">
-          <img class="w-full bg-blue-300 p-6" src="/icon.png" alt="Sunset in the mountains">
+        <div class="flex-1 w-4/12 sm:w-4/12 md:w-2/12">
+          <img class="w-full bg-dark-green-c py-3 px-5 md:px-16 md:py-8 panel-img" src="/images/events/bash.svg" alt="Sunset in the mountains">
           <div class="text-left">
-            <h6>Event</h6>
+            <h6 class="text-px-21 black">Networking Bash</h6>
           </div>
         </div>
-        <div class="w-4/12 sm:w-4/12 md:w-2/12">
-          <img class="w-full bg-blue-300 p-6" src="/icon.png" alt="Sunset in the mountains">
+        <div class="flex-1 w-4/12 sm:w-4/12 md:w-2/12">
+          <img class="w-full bg-purple-c py-3 px-5 md:px-16 md:py-8 panel-img" src="/images/events/hack.svg" alt="Sunset in the mountains">
           <div class="text-left">
-            <h6>Event</h6>
+            <h6 class="text-px-21 black">HackBounty</h6>
           </div>
         </div>
       </div>
@@ -55,5 +55,7 @@ export default {
 </script>
 
 <style scoped>
-
+.panel-img {
+  height: 140px;
+}
 </style>
