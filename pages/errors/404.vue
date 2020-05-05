561c5ea80ec77d22a3c536b340ec8098124ee37c
manuelgeek <emashmagak@appslab.co.ke>
2020-05-06 02:28:16 +0300

    fixed lints

--- pages/errors/404.vue
+++ pages/errors/404.vue
@@ -4,9 +4,15 @@
       <div class="w-full flex flex-wrap px-4 md:px-12 py-0 md:py-3 items-center justify-end">
         <div class="content-center w-full lg:w-1/2 flex">
           <div class=" text-center">
-            <p class="text-px-13-slab black uppercase font-bold">ERROR</p>
-            <h1 class="error-font">404</h1>
-            <h5 class="title-px-21 black px-2 md:px-5 mt-4 md:mt-10 mb-4 md:mb-10">you can stay here and look at the cookies or head back home</h5>
+            <p class="text-px-13-slab black uppercase font-bold">
+              ERROR
+            </p>
+            <h1 class="error-font">
+              404
+            </h1>
+            <h5 class="title-px-21 black px-2 md:px-5 mt-4 md:mt-10 mb-4 md:mb-10">
+              you can stay here and look at the cookies or head back home
+            </h5>
             <div class="w-full px-2 md:px-10 mb-4 md:mb-10">
               <count-days />
             </div>
